a5837b3c7e1ad381c52164236b04c8d5c4ef38f2
coderkhem <khemagarwal1@gmail.com>
2020-06-02 23:17:08 +0530

    done

--- timeline.js
+++ timeline.js
@@ -19,7 +19,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 docRef.get().then(function(doc) {
   if (doc.exists) {
-      console.log("Document data full data:", doc.data());
+      console.log("Document data:", doc.data());
   
 
     document.getElementById("Idea").innerHTML=doc.data().Idea;
